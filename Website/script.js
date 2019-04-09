c1e9eb279eee8ea75e045c12e8d33fc2a505ada1
sojarv <vs3598@student.uni-lj.si>
2019-04-09 14:22:30 +0200

    Website
    Website 1.0

---
+++ Website/script.js
@@ -0,0 +1,8 @@
+$( "input" ).focusout(function() {
+                               
+    var r = document.getElementById('red').value;
+    var g = document.getElementById('green').value;
+    var b = document.getElementById('blue').value;
+    document.getElementById('box').style.background = "rgb("+r+","+g+","+b+")";
+    
+  });
